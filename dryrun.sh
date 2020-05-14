127430940b71568b15bf27dd1e885e59c6a4863d
Leah Kemp <leah.kemp@esr.cri.nz>
2020-05-14 12:14:48 +1200

    Minor

--- dryrun.sh
+++ dryrun.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -x
 
-snakemake -n -j 24 --use-conda --use-singularity --singularity-args '-B /dir/to/databases/' --configfile config.yaml
+snakemake \
+-n -j 24 \
+--use-conda \
+--use-singularity \
+--singularity-args '-B /dir/to/databases/' \
+--configfile config.yaml
--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -x
 
-snakemake -j 24 --use-conda --use-singularity --singularity-args '-B /dir/to/databases/' --configfile config.yaml
+snakemake \
+-j 24 \
+--use-conda \
+--use-singularity \
+--singularity-args '-B /dir/to/databases/' \
+--configfile config.yaml
